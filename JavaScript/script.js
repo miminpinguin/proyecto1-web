a10db63bc217c3422c9c62dd9ba5d9848e72f78b
miminpinguin <josemiguelcardonasolis@gmail.com>
2025-01-28 13:44:20 -0600

    JavaScript
    se agregó la carpeta de JavaScript

---
+++ JavaScript/script.js
@@ -0,0 +1,7 @@
+//obtener el boton por su ID que le puse
+const boton=document.getElementById('botoncito');
+//agregar un evento al boton
+boton.addEventListener('click', function(){
+    alert("hola,le acabas de dar click. psdt: se descargó un troyano");
+
+});
