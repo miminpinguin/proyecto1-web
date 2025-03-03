14010464415ea10ecaee63b0512a46027241945f
miminpinguin <josemiguelcardonasolis@gmail.com>
2025-03-03 14:16:27 -0600

    se agregó el proyecto

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,6 @@
-//obtener el boton por su ID que le puse
 const boton=document.getElementById('botoncito');
 //agregar un evento al boton
 boton.addEventListener('click', function(){
-    alert("hola,le acabas de dar click. psdt: se descargó un troyano");
+    alert("hola, acabas de ingresar a la mejor pagina de filosofia");
 
 });
